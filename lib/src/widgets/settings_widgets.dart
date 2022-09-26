ad04b41d7927a2c623b21e3d6b7da4b2bb96a3ab
jhkim <jhkim@mbc.co.kr>
2022-09-26 09:15:04 +0900

    add childPadding parameter

--- lib/src/widgets/settings_widgets.dart
+++ lib/src/widgets/settings_widgets.dart
@@ -295,23 +295,24 @@ class SettingsContainer extends StatelessWidget {
 
   final double leftPadding;
 
+  final EdgeInsets childPadding;
+
   SettingsContainer({
     required this.children,
     this.allowScrollInternally = false,
     this.leftPadding = 0.0,
+    this.childPadding = const EdgeInsets.only(top: 16.0),
   });
 
   @override
   Widget build(BuildContext context) {
-    return _buildChild();
+    return _buildChild(childPadding);
   }
 
-  Widget _buildChild() {
+  Widget _buildChild(childPadding) {
     var child = allowScrollInternally ? getList(children) : getColumn(children);
     return Padding(
-      padding: EdgeInsets.only(
-        top: 16.0,
-      ),
+      padding: childPadding,
       child: Material(
         child: Container(
           padding: EdgeInsets.only(left: leftPadding),
@@ -584,7 +585,8 @@ class _TextInputSettingsTileState extends State<TextInputSettingsTile> {
     _focusNode = FocusNode();
     _focusNode.addListener(() {
       if (widget.selectAllOnFocus) {
-        _controller.selection = TextSelection(baseOffset: 0, extentOffset: _controller.text.length);
+        _controller.selection =
+            TextSelection(baseOffset: 0, extentOffset: _controller.text.length);
       }
     });
   }
@@ -594,7 +596,8 @@ class _TextInputSettingsTileState extends State<TextInputSettingsTile> {
     return ValueChangeObserver<String>(
       cacheKey: widget.settingKey,
       defaultValue: widget.initialValue,
-      builder: (BuildContext context, String value, OnChanged<String> onChanged) {
+      builder:
+          (BuildContext context, String value, OnChanged<String> onChanged) {
         WidgetsBinding.instance.addPostFrameCallback((_) {
           _controller.text = value;
         });
@@ -621,7 +624,8 @@ class _TextInputSettingsTileState extends State<TextInputSettingsTile> {
     );
   }
 
-  Widget _buildTextField(BuildContext context, String value, OnChanged<String> onChanged) {
+  Widget _buildTextField(
+      BuildContext context, String value, OnChanged<String> onChanged) {
     final borderColor = widget.borderColor ?? Colors.blue;
     final errorColor = widget.errorColor ?? Colors.red;
 
@@ -832,7 +836,8 @@ class SwitchSettingsTile extends StatelessWidget {
     );
   }
 
-  Future<void> _onSwitchChange(BuildContext context, bool? value, OnChanged<bool> onChanged) async {
+  Future<void> _onSwitchChange(
+      BuildContext context, bool? value, OnChanged<bool> onChanged) async {
     if (value == null) return;
     onChanged(value);
     onChange?.call(value);
@@ -852,7 +857,8 @@ class SwitchSettingsTile extends StatelessWidget {
     return label;
   }
 
-  Widget getFinalWidget(BuildContext context, Widget mainWidget, bool currentValue, List<Widget>? childrenIfEnabled) {
+  Widget getFinalWidget(BuildContext context, Widget mainWidget,
+      bool currentValue, List<Widget>? childrenIfEnabled) {
     if (childrenIfEnabled == null || !currentValue) {
       return SettingsContainer(
         children: [mainWidget],
@@ -1015,7 +1021,8 @@ class CheckboxSettingsTile extends StatelessWidget {
     return label;
   }
 
-  Widget getFinalWidget(BuildContext context, Widget mainWidget, bool currentValue, List<Widget>? childrenIfEnabled) {
+  Widget getFinalWidget(BuildContext context, Widget mainWidget,
+      bool currentValue, List<Widget>? childrenIfEnabled) {
     if (childrenIfEnabled == null || !currentValue) {
       return SettingsContainer(
         children: [mainWidget],
@@ -1148,7 +1155,9 @@ class _RadioSettingsTileState<T> extends State<RadioSettingsTile<T>> {
               visible: showTitles,
               child: _SimpleHeaderTile(
                 title: widget.title,
-                subtitle: widget.subtitle.isNotEmpty ? widget.subtitle : widget.values[selectedValue],
+                subtitle: widget.subtitle.isNotEmpty
+                    ? widget.subtitle
+                    : widget.values[selectedValue],
                 leading: widget.leading,
                 titleTextStyle: widget.titleTextStyle,
                 subtitleTextStyle: widget.subtitleTextStyle,
@@ -1163,8 +1172,10 @@ class _RadioSettingsTileState<T> extends State<RadioSettingsTile<T>> {
 
   bool get showTitles => widget.showTitles;
 
-  Widget _buildRadioTiles(BuildContext context, T groupValue, OnChanged<T> onChanged) {
-    var radioList = widget.values.entries.map<Widget>((MapEntry<T, String> entry) {
+  Widget _buildRadioTiles(
+      BuildContext context, T groupValue, OnChanged<T> onChanged) {
+    var radioList =
+        widget.values.entries.map<Widget>((MapEntry<T, String> entry) {
       return _SettingsTile(
         title: entry.value,
         titleTextStyle: widget.titleTextStyle,
@@ -1272,7 +1283,8 @@ class DropDownSettingsTile<T> extends StatefulWidget {
   });
 
   @override
-  _DropDownSettingsTileState<T> createState() => _DropDownSettingsTileState<T>();
+  _DropDownSettingsTileState<T> createState() =>
+      _DropDownSettingsTileState<T>();
 }
 
 class _DropDownSettingsTileState<T> extends State<DropDownSettingsTile<T>> {
@@ -1303,7 +1315,8 @@ class _DropDownSettingsTileState<T> extends State<DropDownSettingsTile<T>> {
                 selected: value,
                 alignment: widget.alignment,
                 values: widget.values.keys.toList().cast<T>(),
-                onChanged: (newValue) => _handleDropDownChange(newValue, onChanged),
+                onChanged: (newValue) =>
+                    _handleDropDownChange(newValue, onChanged),
                 enabled: widget.enabled,
                 itemBuilder: (T value) {
                   return Text(widget.values[value]!);
@@ -1478,21 +1491,27 @@ class _SliderSettingsTileState extends State<SliderSettingsTile> {
     return ValueChangeObserver<double>(
       cacheKey: widget.settingKey,
       defaultValue: currentValue,
-      builder: (BuildContext context, double value, OnChanged<double> onChanged) {
+      builder:
+          (BuildContext context, double value, OnChanged<double> onChanged) {
         // debugPrint('creating settings Tile: ${widget.settingKey}');
         return SettingsContainer(
           children: <Widget>[
             _SimpleHeaderTile(
               title: widget.title,
-              subtitle: widget.subtitle.isNotEmpty ? widget.subtitle : value.toStringAsFixed(widget.decimalPrecision),
+              subtitle: widget.subtitle.isNotEmpty
+                  ? widget.subtitle
+                  : value.toStringAsFixed(widget.decimalPrecision),
               leading: widget.leading,
               titleTextStyle: widget.titleTextStyle,
               subtitleTextStyle: widget.subtitleTextStyle,
             ),
             _SettingsSlider(
-              onChanged: (newValue) => _handleSliderChanged(newValue, onChanged),
-              onChangeStart: (newValue) => _handleSliderChangeStart(newValue, onChanged),
-              onChangeEnd: (newValue) => _handleSliderChangeEnd(newValue, onChanged),
+              onChanged: (newValue) =>
+                  _handleSliderChanged(newValue, onChanged),
+              onChangeStart: (newValue) =>
+                  _handleSliderChangeStart(newValue, onChanged),
+              onChangeEnd: (newValue) =>
+                  _handleSliderChangeEnd(newValue, onChanged),
               enabled: widget.enabled,
               eagerUpdate: widget.eagerUpdate,
               value: value,
@@ -1522,7 +1541,8 @@ class _SliderSettingsTileState extends State<SliderSettingsTile> {
     widget.onChangeStart?.call(newValue);
   }
 
-  Future<void> _handleSliderChangeEnd(double newValue, OnChanged<double> onChanged) async {
+  Future<void> _handleSliderChangeEnd(
+      double newValue, OnChanged<double> onChanged) async {
     _updateWidget(newValue, onChanged);
     widget.onChangeEnd?.call(newValue);
   }
@@ -1598,7 +1618,8 @@ class ColorPickerSettingsTile extends StatefulWidget {
   });
 
   @override
-  _ColorPickerSettingsTileState createState() => _ColorPickerSettingsTileState();
+  _ColorPickerSettingsTileState createState() =>
+      _ColorPickerSettingsTileState();
 }
 
 class _ColorPickerSettingsTileState extends State<ColorPickerSettingsTile> {
@@ -1620,7 +1641,8 @@ class _ColorPickerSettingsTileState extends State<ColorPickerSettingsTile> {
     return ValueChangeObserver<String>(
       cacheKey: widget.settingKey,
       defaultValue: currentValue,
-      builder: (BuildContext context, String value, OnChanged<String> onChanged) {
+      builder:
+          (BuildContext context, String value, OnChanged<String> onChanged) {
         // debugPrint('creating settings Tile: ${widget.settingKey}');
         return _SettingsColorPicker(
           title: widget.title,
@@ -1636,7 +1658,8 @@ class _ColorPickerSettingsTileState extends State<ColorPickerSettingsTile> {
     );
   }
 
-  Future<void> _handleColorChanged(String color, OnChanged<String> onChanged) async {
+  Future<void> _handleColorChanged(
+      String color, OnChanged<String> onChanged) async {
     currentValue = color;
     onChanged(color);
     final colorFromString = ConversionUtils.colorFromString(color);
@@ -1722,7 +1745,8 @@ class RadioModalSettingsTile<T> extends StatefulWidget {
   });
 
   @override
-  _RadioModalSettingsTileState<T> createState() => _RadioModalSettingsTileState<T>();
+  _RadioModalSettingsTileState<T> createState() =>
+      _RadioModalSettingsTileState<T>();
 }
 
 class _RadioModalSettingsTileState<T> extends State<RadioModalSettingsTile<T>> {
@@ -1751,7 +1775,9 @@ class _RadioModalSettingsTileState<T> extends State<RadioModalSettingsTile<T>> {
           children: [
             _ModalSettingsTile<T>(
               title: widget.title,
-              subtitle: widget.subtitle.isNotEmpty ? widget.subtitle : widget.values[value],
+              subtitle: widget.subtitle.isNotEmpty
+                  ? widget.subtitle
+                  : widget.values[value],
               leading: widget.leading,
               titleTextStyle: widget.titleTextStyle,
               subtitleTextStyle: widget.subtitleTextStyle,
@@ -1882,7 +1908,8 @@ class SliderModalSettingsTile extends StatefulWidget {
   });
 
   @override
-  _SliderModalSettingsTileState createState() => _SliderModalSettingsTileState();
+  _SliderModalSettingsTileState createState() =>
+      _SliderModalSettingsTileState();
 }
 
 class _SliderModalSettingsTileState extends State<SliderModalSettingsTile> {
@@ -1899,21 +1926,27 @@ class _SliderModalSettingsTileState extends State<SliderModalSettingsTile> {
     return ValueChangeObserver<double>(
       cacheKey: widget.settingKey,
       defaultValue: currentValue,
-      builder: (BuildContext context, double value, OnChanged<double> onChanged) {
+      builder:
+          (BuildContext context, double value, OnChanged<double> onChanged) {
         // debugPrint('creating settings Tile: ${widget.settingKey}');
         return SettingsContainer(
           children: <Widget>[
             _ModalSettingsTile<double>(
               title: widget.title,
-              subtitle: widget.subtitle.isNotEmpty ? widget.subtitle : value.toString(),
+              subtitle: widget.subtitle.isNotEmpty
+                  ? widget.subtitle
+                  : value.toString(),
               leading: widget.leading,
               titleTextStyle: widget.titleTextStyle,
               subtitleTextStyle: widget.subtitleTextStyle,
               children: <Widget>[
                 _SettingsSlider(
-                  onChanged: (double newValue) => _handleSliderChanged(newValue, onChanged),
-                  onChangeStart: (double newValue) => _handleSliderChangeStart(newValue, onChanged),
-                  onChangeEnd: (double newValue) => _handleSliderChangeEnd(newValue, onChanged),
+                  onChanged: (double newValue) =>
+                      _handleSliderChanged(newValue, onChanged),
+                  onChangeStart: (double newValue) =>
+                      _handleSliderChangeStart(newValue, onChanged),
+                  onChangeEnd: (double newValue) =>
+                      _handleSliderChangeEnd(newValue, onChanged),
                   enabled: widget.enabled,
                   eagerUpdate: widget.eagerUpdate,
                   value: value,
